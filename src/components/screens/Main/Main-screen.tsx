3d4010c0544a44d89891eb353fc3588c3dcc21c5
Yauheny Vial <mr.jk1629003@gmail.com>
2023-06-08 23:33:11 +0300

    feat: add icons and fonts

--- src/components/screens/Main/Main-screen.tsx
+++ src/components/screens/Main/Main-screen.tsx
@@ -6,7 +6,7 @@ import styles from './Main-screen.module.scss';
 export const MainScreen: FC = () => {
     return (
         <section className={styles.mainScreen}>
-            MainScreen
+           <p>MainScreen</p>
         </section>
     )
 };
